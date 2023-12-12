68d3cfea1eabe95758d118586a0d80f2f5bdce2a
丘光义 <qiuguangyi@52tt.com>
2023-12-12 22:10:04 +0800

    feat: 加入静态资源

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+push_addr=`git remote get-url --push origin`
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist
+push_branch=gh-pages
+
+dir_exists() {
+  node -e "var fs = require('fs');var path = require('path'); console.log(fs.existsSync(path.resolve(__dirname,'$1')))"
+}
+
+rm_dist(){
+  cd -
+  if [ "$(dir_exists $dist_path)" = "true" ]; then
+    echo "remove $dist_path"
+    # chmod -R 777 $dist_path
+    rm -rf $dist_path
+  fi
+}
+
+handle_error(){
+  rm_dist
+}
+
+# 确保脚本抛出遇到的错误
+set -ex
+
+# 错误的时候执行
+trap "handle_error" ERR
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git add -A
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
+
+rm_dist
