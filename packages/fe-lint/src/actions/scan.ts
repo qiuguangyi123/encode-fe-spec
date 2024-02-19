8ab3e8b1d08e9d0e41876dbc2dfd3644f578ab1b
丘光义 <qiuguangyi@52tt.com>
2024-02-19 22:15:35 +0800

    feat: scan eslint

--- packages/fe-lint/src/actions/scan.ts
+++ packages/fe-lint/src/actions/scan.ts
@@ -1,8 +1,15 @@
 import path from 'path';
-import type { ScanReport, ScanOptions, PKG, InitOptions } from '../types';
+import type { ScanReport, ScanOptions, PKG, InitOptions, ScanResult } from '../types';
 import fs from 'fs-extra';
+import { PKG_NAME } from '../config/constants';
 export default async (options: ScanOptions): Promise<ScanReport> => {
-  const { cwd, outputReport, config } = options;
+  const { cwd, outputReport, config, fix } = options;
   const pkg: PKG = fs.readJSONSync(path.resolve(cwd, 'package.json'));
-  let scanConfig: Omit<InitOptions, 'cwd' | 'checkVersionUpdate' | 'disableNpmInstall' | 'rewriteConfig'>;
+  let scanConfig: Omit<InitOptions, 'cwd' | 'checkVersionUpdate' | 'disableNpmInstall' | 'rewriteConfig'> =
+    config || fs.readJSONSync(path.resolve(cwd, `${PKG_NAME.replace(/@.*\//, '')}.config.js`));
+  let results: ScanResult[] = [];
+  if (scanConfig.enableESLint) {
+    const eslintResults: ScanResult[] = await doESlint({ ...options, pkg, config: scanConfig });
+    results.push(...eslintResults);
+  }
 };
