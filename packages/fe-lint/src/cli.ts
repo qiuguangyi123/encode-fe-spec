bbee34d203ecf722d42ab5fd1bbcfb88dda2d744
13544345228 <1007759860@qq.com>
2024-03-03 21:58:01 +0800

    feat: wc

--- packages/fe-lint/src/cli.ts
+++ packages/fe-lint/src/cli.ts
@@ -106,11 +106,10 @@ program
   .action(async () => {
     try {
       const result = spawn.sync('commitlint', ['-E', 'HUSKY_GIT_PARAMS'], { stdio: 'inherit' });
-      console.log(result, 666);
-      if (result.status !== 0) {
-        log.error('commit message 格式不正确，请检查后重新提交');
-        process.exit(result.status);
-      }
+      // if (result.status !== 0) {
+      //   log.error('commit message 格式不正确，请检查后重新提交');
+      //   process.exit(result.status);
+      // }
       // const s = await execa('commitlint', ['-E', 'HUSKY_GIT_PARAMS']);
       // console.log(process.env.HUSKY_GIT_PARAMS, s, 1111);
       // process.exit(1);
